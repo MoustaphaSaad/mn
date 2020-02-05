4addb370b79e09d5c7b6e78dc47c65263056777b
IslamMagdy360 <42382262+IslamMagdy360@users.noreply.github.com>
2020-02-05 20:51:05 +0200

    fix ipc problem with a file as mutex

--- mn/src/mn/mac/IPC.cpp
+++ mn/src/mn/mac/IPC.cpp
@@ -2,198 +2,73 @@
 #include "mn/Str.h"
 #include "mn/Memory.h"
 #include "mn/Fabric.h"
+#include "mn/File.h"
 
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <atomic>
 #include <assert.h>
 
 namespace mn::ipc
 {
-	struct IShared_Mutex
-	{
-		pthread_mutex_t mtx;
-		std::atomic<int32_t> atomic_rc;
-	};
 
-	struct IIPC_Mutex
-	{
-		IShared_Mutex* shared_mtx;
-		int shm_fd;
-		Str name;
-		bool created;
-	};
+    struct IIPC_Mutex
+    {
+        mn::File mtx;
+    };
 
-	Mutex
-	mutex_new(const Str& name)
-	{
-		errno = 0;
-		// Open existing shared memory object, or create one.
-		// Two separate calls are needed here, to mark fact of creation
-		// for later initialization of pthread mutex.
-		int shm_fd = shm_open(name.ptr, O_RDWR, 0660);
-		bool created = false;
+    Mutex
+    mutex_new(const Str& name)
+    {
 
-        // mutex was not created be  fore, create it!
-		if(errno == ENOENT)
-		{
-			shm_fd = shm_open(name.ptr, O_RDWR | O_CREAT, 0660);
-			created = true;         
+        auto mtx = mn::file_open(name, mn::IO_MODE::WRITE, mn::OPEN_MODE::CREATE_APPEND);
+        if(mtx == nullptr)
+            return  nullptr;
 
-            // Truncate shared memory segment so it would contain IShared_Mutex
-            if (ftruncate(shm_fd, sizeof(IShared_Mutex) != 0))
-            {
-                close(shm_fd);
-                shm_unlink(name.ptr);
-                return nullptr;
-            }
-		}
+        auto self = alloc<IIPC_Mutex>();
+        self->mtx = mtx;
+        return self;
+    }
 
-		if(shm_fd == -1)
-			return nullptr;
+    void
+    mutex_free(Mutex self)
+    {
+       self->mtx->dispose();
 
-        // Map pthread mutex into the shared memory.
-		void *addr = mmap(
-		  NULL,
-		  sizeof(IShared_Mutex),
-		  PROT_READ|PROT_WRITE,
-		  MAP_SHARED,
-		  shm_fd,
-		  0
-		);
+       mn::free(self);
+    }
 
-		if(addr == MAP_FAILED)
-		{
-			close(shm_fd);
-            shm_unlink(name.ptr);
-			return nullptr;
-		}
+    void
+    mutex_lock(Mutex self)
+    {
+        worker_block_ahead();
 
-		auto shared_mtx = (IShared_Mutex*)addr;
+        file_write_lock(self->mtx, 0, 0);
 
-		// if first one to create the mutex then initialize it!
-		if(created)
-		{
-			pthread_mutexattr_t attr{};
-			[[maybe_unused]] auto mtx_err = pthread_mutexattr_init(&attr);
-			assert(mtx_err == 0);
+        worker_block_clear();
+    }
 
-			mtx_err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
-			assert(mtx_err == 0);	
+    LOCK_RESULT
+    mutex_try_lock(Mutex self)
+    {
+        bool res = file_write_try_lock(self->mtx, 0, 0);
 
-		//	mtx_err = pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST);
-		//	assert(mtx_err == 0);
+        if(res)
+        {
+            return  LOCK_RESULT::OBTAINED;
+        }
+        else
+        {
+            return LOCK_RESULT::FAILED;
+        }
 
-			mtx_err = pthread_mutex_init(&shared_mtx->mtx, &attr);
-			assert(mtx_err == 0);
+    }
 
-			shared_mtx->atomic_rc = 1;
-		}
-		else
-		{
-			shared_mtx->atomic_rc.fetch_add(1);
-		}
-
-		auto self = alloc<IIPC_Mutex>();
-		self->shared_mtx = shared_mtx;
-		self->shm_fd = shm_fd;
-		self->name = clone(name);
-		self->created = created;
-
-		return self;
-	}
-
-	void
-	mutex_free(Mutex self)
-	{
-		if(self->shared_mtx->atomic_rc.fetch_sub(1) <= 1)
-		{
-			[[maybe_unused]] int res = pthread_mutex_destroy(&self->shared_mtx->mtx);
-			assert(res == 0);
-
-			res = munmap(self->shared_mtx, sizeof(self->shared_mtx));
-			assert(res == 0);
-
-			res = close(self->shm_fd);
-			assert(res == 0);
-
-			res = shm_unlink(self->name.ptr);
-			assert(res == 0);
-
-			str_free(self->name);
-
-			mn::free(self);
-		}
-		else
-		{
-			[[maybe_unused]] int res = munmap(self->shared_mtx, sizeof(self->shared_mtx));
-			assert(res == 0);
-
-			res = close(self->shm_fd);
-			assert(res == 0);
-
-			str_free(self->name);
-
-			mn::free(self);
-		}
-	}
-
-	void
-	mutex_lock(Mutex self)
-	{
-		worker_block_ahead();
-		while(true)
-		{
-			int res = pthread_mutex_lock(&self->shared_mtx->mtx);
-			if(res == EOWNERDEAD)
-			{
-				//pthread_mutex_consistent(&self->shared_mtx->mtx);
-				res = pthread_mutex_unlock(&self->shared_mtx->mtx);
-				assert(res == 0);
-				self->shared_mtx->atomic_rc.fetch_sub(1);
-				continue;
-			}
-			assert(res == 0);
-			break;
-		}
-		worker_block_clear();
-	}
-
-	LOCK_RESULT
-	mutex_try_lock(Mutex self)
-	{
-		int res = pthread_mutex_trylock(&self->shared_mtx->mtx);
-		if(res == 0)
-		{
-			return LOCK_RESULT::OBTAINED;
-		}
-		else if (res == EOWNERDEAD)
-		{
-			//pthread_mutex_consistent(&self->shared_mtx->mtx);
-			res = pthread_mutex_unlock(&self->shared_mtx->mtx);
-			assert(res == 0);
-			self->shared_mtx->atomic_rc.fetch_sub(1);
-
-			res = pthread_mutex_trylock(&self->shared_mtx->mtx);
-			if(res == 0)
-				return LOCK_RESULT::ABANDONED;
-			else
-				return LOCK_RESULT::FAILED;
-		}
-		else
-		{
-			return LOCK_RESULT::FAILED;
-		}
-	}
-
-	void
-	mutex_unlock(Mutex self)
-	{
-		[[maybe_unused]] int res = pthread_mutex_unlock(&self->shared_mtx->mtx);	
-		assert(res == 0);
-	}
+    void
+    mutex_unlock(Mutex self)
+    {
+        file_write_unlock(self->mtx, 0, 0);
+    }
 }
